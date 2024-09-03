b48cff6f9d1c3dde46eb4f9e48315b23e7889713
alexgicimo <gakurualexgicimo@gmail.com>
2024-09-03 14:52:44 +0300

    create a three turtle snake

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
+from turtle import Screen
+from snake_game import Snake
 def main() -> None:
-    pass
+    screen = Screen()
+    snake = Snake()
+    screen.setup(width=800, height=600)
+    screen.bgcolor("black")
+    screen.title("Snake Game")
+    
+    
+    
+    
+    screen.exitonclick()
+
 
 if __name__ == "__main__":
     main()
--- snake_game.py
+++ snake_game.py
@@ -3,9 +3,15 @@ STARTING_COORD = [(0,0), (20, 0), (40, 0)]
 class Snake:
     def __init__(self):
         self.snakes = []
+        self.head = STARTING_COORD[0]
+        self.snake()
 
     def snake(self):
         for coord in STARTING_COORD:
             turtle = Turtle()
+            turtle.shape("square")
+            turtle.color("white")
+            turtle.penup()
             turtle.goto(coord)
             self.snakes.append(turtle)
+        
