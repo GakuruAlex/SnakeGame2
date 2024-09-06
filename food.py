449ddff666695fbc2ab34ae6c427d7663a45ee61
alexgicimo <gakurualexgicimo@gmail.com>
2024-09-06 12:56:30 +0300

    Generate food at a random location

--- food.py
+++ food.py
@@ -1,7 +1,15 @@
 from turtle import Turtle
-from random import random
+from random import randint
 
 class Food(Turtle):
-    def __init__self():
+    def __init__(self):
         super().__init__()
-        
+        self.color("red")
+        self.shape("circle")
+        self.shapesize(stretch_len=0.5, stretch_wid= 0.5)
+        self.penup()
+        self.make_food()
+    def make_food(self):
+        x_cor = randint(-380, 380)
+        y_cor = randint(-280, 280)
+        self.goto(x= x_cor, y= y_cor)
