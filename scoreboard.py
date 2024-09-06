9f6c10cddafd0bf7b9be3583a37b5f4d71d74e57
alexgicimo <gakurualexgicimo@gmail.com>
2024-09-06 16:17:39 +0300

    read and write high score to highscore txt

--- scoreboard.py
+++ scoreboard.py
@@ -10,13 +10,28 @@ class ScoreBoard(Turtle):
         self.goto(0, 260)
         self.hideturtle()
         self.display_score()
+
+    def write_high_score(self, score):
+        with open("high_score.txt", mode="w") as file:
+            file.write(f"HighScore, {score}")
+    def read_high_score(self):
+        try:
+            with open("high_score.txt", mode="r") as file:
+                high_score = file.read().split(",")[1]
+            return int(high_score)
+        except FileNotFoundError:
+            return 0
     def display_score(self):
+        self.high_score = self.read_high_score()
         self.clear()
-        self.write(f"Score: {self.score}", font=FONT)
+        self.write(f"Score: {self.score} HighScore: {self.high_score}", font=FONT)
     def increase_score(self):
         self.score += 1
         self.display_score()
-    def end_game(self):
-        self.clear()
-        self.goto(0,0)
-        self.write(f"Game Over! Your score: {self.score}", font= FONT)
+
+    def reset(self):
+        if self.score > self.read_high_score():
+            self.write_high_score(self.score)
+        self.score = 0
+        self.display_score()
+   
