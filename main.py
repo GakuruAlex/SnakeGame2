32d9452a1b2ceeeb41daf5ed3602dfddbbab70c1
alexgicimo <gakurualexgicimo@gmail.com>
2024-09-06 12:56:47 +0300

    change food position on colliding with snake

--- main.py
+++ main.py
@@ -1,17 +1,19 @@
 from turtle import Screen
 from time import sleep
 from snake_game import Snake
+from food import Food
 def main() -> None:
     screen = Screen()
     screen.tracer(0)
     snake = Snake()
+    food = Food()
     screen.setup(width=800, height=600)
     screen.bgcolor("black")
     screen.title("Snake Game")
     game_is_on = True
 
     while game_is_on:
-        sleep(0.4)
+        sleep(0.2)
         screen.update()
         snake.move_snake()
         screen.listen()
@@ -19,6 +21,8 @@ def main() -> None:
         screen.onkey(snake.move_down,"Down")
         screen.onkey(snake.move_left,"Left")
         screen.onkey(snake.move_right, "Right")
+        if food.distance(snake.head) < 10:
+            food.make_food()
 
     screen.exitonclick()
 
